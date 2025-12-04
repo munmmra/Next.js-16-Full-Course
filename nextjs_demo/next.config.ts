95bd370bd2015476e4b142a5ff9a09a5f0f8262b
munmmra <munmmra@gmail.com>
2025-12-04 06:37:17 -0600

    habilita turbopack

--- nextjs_demo/next.config.ts
+++ nextjs_demo/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // habilitando turbopack para una compilación más rápida
+  experimental: {
+    turbopackFileSystemCacheForDev : true,
+  },
+
 };
 
 export default nextConfig;
